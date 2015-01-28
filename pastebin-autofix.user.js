99a142c62e74a39b655941359f966c0eea4a07ae
Niklas Mollenhauer <nikeee@outlook.com>
2015-01-28 04:39:42 +0100

    Konnichiwa

---
+++ pastebin-autofix.user.js
@@ -0,0 +1,32 @@
+// ==UserScript==
+// @name        Pastebin-Autofix
+// @namespace   Niklas Mollenhauer
+// @description Autosets default expiration/exposure
+// @include     http://pastebin.com/
+// @include     https://pastebin.com/
+// @version     1.0.0
+// @updateURL	https://raw.githubusercontent.com/nikeee/pastebin-autofix/master/pastebin-autofix.user.js
+// @downloadURL https://raw.githubusercontent.com/nikeee/pastebin-autofix/master/pastebin-autofix.user.js
+// @grant       none
+// ==/UserScript==
+
+(function(){
+	var defaultExpire = "1D";
+	var defaultUnlisted = true;
+
+	$(function(){
+		var $select = $("select[name=paste_expire_date]")[0];
+		if($select)
+		{
+			$select.value = defaultExpire;
+		}
+		if(defaultUnlisted)
+		{
+			var $privateStatus = $("select[name=paste_private]")[0];
+			if($privateStatus)
+			{
+				$privateStatus.value = 1;
+			}
+		}
+	});
+})();
